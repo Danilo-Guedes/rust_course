e9e2f151fe1901c26bfd08b9153e695050edb9fb
Danilo Paz Guedes de Freitas <danilotiba@gmail.com>
2024-11-03 15:49:36 -0300

    my answer to the emi calculator exercise

--- hello_world_001/target/debug/deps/hello_world_001-e357e17af0778e13.d
+++
@@ -1,5 +0,0 @@
-/home/dguedes/rust/rust_course/hello_world_001/target/debug/deps/hello_world_001-e357e17af0778e13: src/main.rs
-
-/home/dguedes/rust/rust_course/hello_world_001/target/debug/deps/hello_world_001-e357e17af0778e13.d: src/main.rs
-
-src/main.rs:
--- hello_world_001/target/debug/hello_world_001.d
+++
@@ -1,1 +0,0 @@
-/home/dguedes/rust/rust_course/hello_world_001/target/debug/hello_world_001: /home/dguedes/rust/rust_course/hello_world_001/src/main.rs
