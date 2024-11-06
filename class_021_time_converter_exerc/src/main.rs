685a001e8c75e6aa2c9ac8d6230a74d1d381dec0
Danilo Paz Guedes de Freitas <danilotiba@gmail.com>
2024-11-06 16:35:42 -0300

    made hour formating exercise

---
+++ class_021_time_converter_exerc/src/main.rs
@@ -0,0 +1,23 @@
+/*
+    Convert seconds to HH:MM:SS format
+*/
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+
+    println!("Enter the time of the day in seconds(0 to 86,399):");
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let total_seconds: u32 = input.trim().parse().expect("Input number only without any sign!");
+
+    if total_seconds > 86399 {
+        panic!("Your input should be between 0 to 86,399 ");
+    }
+
+    let hours = total_seconds / 3600;
+    let remaining_seconds = total_seconds % 3600;
+    let minutes = remaining_seconds / 60;
+    let remaining_seconds = remaining_seconds % 60;
+
+    println!("The time in 24hr format is {:02}:{:02}:{:02}", hours, minutes, remaining_seconds);
+}
