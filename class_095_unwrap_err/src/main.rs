8a96a72e868f163353b447a537e80e6093d1a733
Danilo Paz Guedes de Freitas <danilotiba@gmail.com>
2024-11-21 18:37:37 -0300

    class_095_unwrap_err

---
+++ class_095_unwrap_err/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    println!("\n\n----------------------------\n\n");
+
+    // UNWRAP_ERR is the counterpart of unwrap, it panicts if the result is a OK variant and returns it
+
+    let res = add_strings("Hello", "").unwrap_err();
+
+    println!("{res}");
+
+    println!("\n\n----------------------------\n\n");
+
+    // UNWRAP_OR 
+
+    let res2 = add_strings("Hello", "").unwrap_or("ERROR: string cannot be empty".to_string()); 
+
+    println!("{res2}");
+
+    println!("\n\n----------------------------\n\n");
+}
+
+fn add_strings(s1: &str, s2: &str) -> Result<String, String> {
+    if s1.is_empty() || s2.is_empty() {
+        return Err("ERROR: string cannot be empty".to_string());
+    }
+
+    return Ok(format!("{} {}", s1, s2));
+}
