58e4734141ad6ed76f088c7ca7e1439e78b08dae
Danilo Paz Guedes de Freitas <danilotiba@gmail.com>
2024-11-21 18:16:31 -0300

    class_094_unwrap_and_expect

---
+++ class_094_unwrap_and_expect/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    println!("\n\n----------------------------\n\n");
+
+    // UNWRAP fn extracts the value from the Option<T> if it is Some<T> and returns the value, is it is None, it panics
+
+    let res = add_strings("Hello", "World").unwrap();
+
+    println!("{res}");
+
+    println!("\n\n----------------------------\n\n");
+
+    // EXPECT fn extracts the value from the Option<T> if it is Some<T> and returns the value, is it is None, it panics with a CUSTOM MESSAGE
+
+    let res2 = add_strings("Hello", "").expect("ERROR: string cannot be empty");
+
+    println!("{res2}");
+
+
+    println!("\n\n----------------------------\n\n");
+
+    // notice that to confidently use unwrap and expect you need to make SURE your fn or method returns an Option<T> type
+    // if not is better to use pattern matching to make your code more robust and easier to maintain
+
+}
+
+fn add_strings(s1: &str, s2: &str) -> Option<String> {
+    if s1.is_empty() || s2.is_empty() {
+        return None;
+    }
+
+    return Some(format!("{} {}", s1, s2));
+}
+
