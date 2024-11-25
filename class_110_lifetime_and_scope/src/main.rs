c43b4b10b301a01b0736461fee113497370e8bc5
Danilo Paz Guedes de Freitas <danilotiba@gmail.com>
2024-11-24 21:10:43 -0300

    class_110_lifetime_and_scope

---
+++ class_110_lifetime_and_scope/src/main.rs
@@ -0,0 +1,15 @@
+fn extend_string<'a>(original: &'a mut String, data: &str) -> &'a str {
+    original.push_str(" ");
+    original.push_str(data);
+    original
+}
+
+fn main() {
+    println!("\n\n-------------------------\n\n");
+    let mut original = String::from("hello");
+    let result = extend_string(&mut original, "world");
+    println!("the result {}", result);
+    println!("\n\n-------------------------\n\n");
+
+}
+
