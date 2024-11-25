86ba0d8b8528a28e25b62e3c71251a119441cc29
Danilo Paz Guedes de Freitas <danilotiba@gmail.com>
2024-11-24 22:06:05 -0300

    class_113_lifetimes_annotation_with_structs

---
+++ class_113_lifetimes_annotation_with_structs/src/main.rs
@@ -0,0 +1,22 @@
+struct MyStruct<'a, 'b> {
+    data1: &'a str,
+    data2: &'b str,
+}
+
+fn main() {
+    let data1 = String::from("Hello");
+    let data2 = String::from("World");
+
+    let my_struct = MyStruct {
+        data1: &data1,
+        data2: &data2,
+    };
+
+    let ret = fun(&my_struct);
+
+    println!("ret = {}", ret);
+}
+
+fn fun(data: &MyStruct) -> &str {
+    data.data1
+}
