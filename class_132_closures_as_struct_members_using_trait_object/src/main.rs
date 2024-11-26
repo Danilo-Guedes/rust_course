ff0a51ba3eeb05583351b2256cb72334a046a943
Danilo Paz Guedes de Freitas <danilotiba@gmail.com>
2024-11-26 13:18:08 -0300

    class_134_types_of_closures

--- class_132_closures_as_struct_members_using_trait_object/src/main.rs
+++ class_132_closures_as_struct_members_using_trait_object/src/main.rs
@@ -5,8 +5,8 @@ struct MyStruct {
     // Closures don't have a fixed size at compile time because each closure can
     // capture variables in unique ways. Using `Box<dyn Fn(i32) -> i32>` allows us
     // to store the closure as a trait object, which has a fixed size (pointer and metadata).
-    // could also be a &dyn Fn(i32) -> i32 using a reference to the closure
-    // or another smart pointer like Rc or Arc that implements the Defer trait.
+    // could also be a &dyn Fn(i32) -> i32 using a reference to a trait object
+    // or another smart pointer like "Rc" or "Arc" that implements the Defer trait.
     action: Box<dyn Fn(i32) -> i32>,
 }
 
@@ -14,7 +14,7 @@ fn main() {
     println!("\n\n-----------------------\n\n");
     let y = 10;
 
-    let closure = Box::new(move |x| x * y);
+    let closure = Box::new(move |x| x * y); // implement Fn trait because it does not mutate the environment
 
     let s = MyStruct { val: 10, action: closure };
 
---
+++ class_134_types_of_closures/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("\n\n-------------------------\n\n");
+    
+    let y = 1;
+
+    //even though this 2 clore are equal, by the fact they capture the enviroment
+    // the compiler generate differente types for them, so they can't be stored in the same vector
+    // that's why we need to use Box<dyn Fn(i32) -> i32> to store them in the same vector
+
+    let c1: Box<dyn Fn(i32) -> i32> = Box::new(|x| x + y); // the type is not need here, but it's just to show the type
+    let c2 = Box::new(|x| x + y);
+
+    let vec_of_closures: Vec<Box<dyn Fn(i32) -> i32>> = vec![c1, c2];
+
+    println!("{}", vec_of_closures[0](1));
+
+
+    println!("\n\n-------------------------\n\n");
+
+}
