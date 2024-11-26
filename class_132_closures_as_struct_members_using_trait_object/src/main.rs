e291aeb7ec6d6d86492fa70d461ace601afefbd6
Danilo Paz Guedes de Freitas <danilotiba@gmail.com>
2024-11-26 13:01:20 -0300

    class_132_closures_as_struct_members_using_trait_object

---
+++ class_132_closures_as_struct_members_using_trait_object/src/main.rs
@@ -0,0 +1,25 @@
+// Define a struct with a member that stores a closure.
+#[allow(dead_code)]
+struct MyStruct {
+    val: i32,
+    // Closures don't have a fixed size at compile time because each closure can
+    // capture variables in unique ways. Using `Box<dyn Fn(i32) -> i32>` allows us
+    // to store the closure as a trait object, which has a fixed size (pointer and metadata).
+    // could also be a &dyn Fn(i32) -> i32 using a reference to the closure
+    // or another smart pointer like Rc or Arc that implements the Defer trait.
+    action: Box<dyn Fn(i32) -> i32>,
+}
+
+fn main() {
+    println!("\n\n-----------------------\n\n");
+    let y = 10;
+
+    let closure = Box::new(move |x| x * y);
+
+    let s = MyStruct { val: 10, action: closure };
+
+    println!("{}", (s.action)(10)); // Output: 100
+
+    println!("\n\n-----------------------\n\n");
+
+}
