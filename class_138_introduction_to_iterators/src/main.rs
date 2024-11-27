f93fb5f68fa9e00924ed230e2b4ccf7f2a92aca5
Danilo Paz Guedes de Freitas <danilotiba@gmail.com>
2024-11-27 18:38:37 -0300

    class_140_creating_a_custom_iterator

--- class_138_introduction_to_iterators/src/main.rs
+++ class_138_introduction_to_iterators/src/main.rs
@@ -6,38 +6,47 @@ fn main() {
         println!("[i32] -> {}", n);
     }
 
-    println!("{:?}", array_of_numbers); // in this case the array is still valid because i32 implements the Copy trait 
+    println!("{:?}", array_of_numbers); // in this case the array is still valid because i32 implements the Copy trait
 
     println!("\n\n-----------------------\n\n");
 
-    let array_of_strings = ["one".to_string(), "two".to_string(), "three".to_string(), "four".to_string(), "five".to_string(),];
-
+    let array_of_strings = [
+        "one".to_string(),
+        "two".to_string(),
+        "three".to_string(),
+        "four".to_string(),
+        "five".to_string(),
+    ];
 
     for s in array_of_strings {
         println!("[String].into_iter() -> {}", s);
     }
 
-    println!("\n\n-----------------------\n\n");    
+    println!("\n\n-----------------------\n\n");
 
-
-    // println!("{:?}", array_of_strings); 
+    // println!("{:?}", array_of_strings);
     // in this case the array is not valid because String does not implement the Copy trait
-    // and the ownership of the array is moved to the for loop  
+    // and the ownership of the array is moved to the for loop
 
+    //     |
+    // 13  |     let array_of_strings = ["one".to_string(), "two".to_string(), "three".to_string(), "four".to_string(), "five".to_string(),];
+    //     |         ---------------- move occurs because `array_of_strings` has type `[String; 5]`, which does not implement the `Copy` trait
+    // ...
+    // 16  |     for s in array_of_strings {
+    //     |              ---------------- `array_of_strings` moved due to this implicit call to `.into_iter()`
+    // ...
+    // 20  |     println!("{:?}", array_of_strings);
+    //     |                      ^^^^^^^^^^^^^^^^ value borrowed here after move
 
-//     |
-// 13  |     let array_of_strings = ["one".to_string(), "two".to_string(), "three".to_string(), "four".to_string(), "five".to_string(),];
-//     |         ---------------- move occurs because `array_of_strings` has type `[String; 5]`, which does not implement the `Copy` trait
-// ...
-// 16  |     for s in array_of_strings {
-//     |              ---------------- `array_of_strings` moved due to this implicit call to `.into_iter()`
-// ...
-// 20  |     println!("{:?}", array_of_strings); 
-//     |                      ^^^^^^^^^^^^^^^^ value borrowed here after move
+    // BUT WE CAN USER THE .iter() METHOD TO ITERATE OVER THE ARRAY WITHOUT MOVING THE OWNERSHIP how the compiler suggests
 
-// BUT WE CAN USER THE .iter() METHOD TO ITERATE OVER THE ARRAY WITHOUT MOVING THE OWNERSHIP how the compiler suggests
-
-    let array_of_strings_2 = ["one".to_string(), "two".to_string(), "three".to_string(), "four".to_string(), "five".to_string(),];
+    let array_of_strings_2 = [
+        "one".to_string(),
+        "two".to_string(),
+        "three".to_string(),
+        "four".to_string(),
+        "five".to_string(),
+    ];
 
     for s in array_of_strings_2.iter() {
         println!("[String].inter() -> {}", s);
@@ -45,5 +54,5 @@ fn main() {
 
     println!("{:?}", array_of_strings_2); // in this case the array is still valid b because we used the .iter which does not move the ownership of the array
 
-    println!("\n\n-----------------------\n\n");    
+    println!("\n\n-----------------------\n\n");
 }
